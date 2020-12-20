3750f741c6616add66682b7bc0daf993bab07f84
FranVB85 <fjvazquez85@gmail.com>
2020-12-20 18:24:50 +0100

    Primer Commit

---
+++ src/es/studium/refactorizacion/CalculoCostes.java
@@ -0,0 +1,12 @@
+package es.studium.refactorizacion;
+
+public class CalculoCostes {
+
+		private static final int COSTE_TRABAJADORES = 1200;
+
+		public static double costeTrabajadores(double numeroTrabajadores) {
+			return COSTE_TRABAJADORES * numeroTrabajadores;
+		}
+	
+
+}
